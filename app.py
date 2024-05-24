110543d03bb15c7374c56fa70cfae31bde6a94c3
Alex-Teodosiu <mihail.alexandru.teodosiu@gmail.com>
2024-05-24 12:38:50 +0200

    Set Flask to run on port 8000 without host and os

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify
-import os
 
 app = Flask(__name__)
 
@@ -8,6 +7,4 @@ def home():
     return jsonify({"message": "Hello, World!"})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(port=8000)
